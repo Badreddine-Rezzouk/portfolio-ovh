3ac72ee9701345d11ef7c31d7a90dfa280f1f476
Badreddine <badreddinerezzouk@protonmail.com>
2025-03-11 22:05:54 +0100

    Ajout de l'image du diplome du Bac, intégration de jQuery dans les headers

--- FR-fr/Academics.php
+++ FR-fr/Academics.php
@@ -9,10 +9,20 @@
 <body>
 <?php require "../Common-files/navbar.php" ?>
 <div class="gradient-box">
-    <div class="bg-white p-4 rounded-3 text-center w-75 offset-1 border border-5 shadow-lg">
-        <h2> Baccalauréat Technologique <abbr title="Sciences Technologiques de l'Industrie et du Développement Durable">STI2D</abbr></h2>
-        <p> Spécialité <abbr title="Sciences informatiques du numérique">SIN</abbr></p>
+    <div class="box"></div>
+    <div class="bg-white p-4 rounded-3 w-75 offset-1 border border-5 shadow-lg">
+        <div class="row">
+            <div class="col" style="min-width: 50%">
+                <img src="../Images/diplome_bac.jpg" class="img-fluid rounded-3 border shadow-lg">
+            </div>
+            <div class="col mxy-auto">
+                <h2> Baccalauréat Technologique <abbr title="Sciences Technologiques de l'Industrie et du Développement Durable">STI2D</abbr></h2>
+                <p> Spécialité <abbr title="Sciences informatiques du numérique">SIN</abbr></p>
+            </div>
+        </div>
+        <p> Diplôme national de niveau IV </p>
     </div>
+    <div class="box"></div>
 </div>
 </body>
 </html>
