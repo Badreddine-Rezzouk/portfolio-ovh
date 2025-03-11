ba028faa5ea94c90a4ee2b9be3e6597ca2efcf69
Badreddine <badreddinerezzouk@protonmail.com>
2025-03-11 20:18:50 +0100

    Initialisation page "Academics" en fr + modification offcanvas

--- FR-fr/Academics.php
+++ FR-fr/Academics.php
@@ -9,7 +9,10 @@
 <body>
 <?php require "../Common-files/navbar.php" ?>
 <div class="gradient-box">
-
+    <div class="bg-white p-4 rounded-3 text-center w-75 offset-1 border border-5 shadow-lg">
+        <h2> Baccalauréat Technologique <abbr title="Sciences Technologiques de l'Industrie et du Développement Durable">STI2D</abbr></h2>
+        <p> Spécialité <abbr title="Sciences informatiques du numérique">SIN</abbr></p>
+    </div>
 </div>
 </body>
 </html>
