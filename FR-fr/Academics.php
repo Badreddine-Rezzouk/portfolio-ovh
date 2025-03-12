db4d83aae0765b3165152c3ed7bae912c02ea115
Badreddine <badreddinerezzouk@protonmail.com>
2025-03-12 08:39:48 +0100

    Restructuration div diplome

--- FR-fr/Academics.php
+++ FR-fr/Academics.php
@@ -10,12 +10,16 @@
 <?php require "../Common-files/navbar.php" ?>
 <div class="gradient-box">
     <div class="box"></div>
-    <div class="bg-white p-4 rounded-3 w-75 offset-1 border border-5 shadow-lg">
+    <div class="bg-white p-4 rounded-3 w-50 mx-auto border border-5 shadow-lg">
+        <h2> Mes diplômes et certifications </h2>
+    </div>
+    <hr class="mx-auto rounded-2">
+    <div class="bg-white p-4 rounded-3 text-start w-75 offset-1 border border-5 shadow-lg">
         <div class="row">
             <div class="col" style="min-width: 50%">
                 <img src="../Images/diplome_bac.jpg" class="img-fluid rounded-3 border shadow-lg">
             </div>
-            <div class="col mxy-auto">
+            <div class="col text-center mx-auto my-auto">
                 <h2> Baccalauréat Technologique <abbr title="Sciences Technologiques de l'Industrie et du Développement Durable">STI2D</abbr></h2>
                 <p> Spécialité <abbr title="Sciences informatiques du numérique">SIN</abbr></p>
             </div>
