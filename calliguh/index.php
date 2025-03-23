d14701cf339a46a2cf5c6ef03ff8e6e52979fc9c
Badreddine <badreddinerezzouk@protonmail.com>
2025-03-23 16:45:57 +0100

    Réimplémentation du sous-projet "calliguh" en standard

---
+++ calliguh/index.php
@@ -0,0 +1,33 @@
+<?php global $language_error_fr;
+require "../Common-files/redirect.php";
+require "../Common-files/unsupportedlanguage.php";
+//require "increment.php";
+?>
+
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <?php require "../Common-files/header.php"?>
+    </head>
+    <body >
+        <div class="container-fluid">
+            <div class="box"></div>
+            <div class="bg-black w-50 mx-auto align-items-center d-flex flex-column justify-content-center" >
+                <form method="POST" action="">
+                    <button type="submit" value="update">
+                        <img src="mori-calliope-hololive.gif" alt="Mori Calliope gif">
+                    </button>
+                    <span id="global_count_value"></span>
+                    <span id="local_countvalue"></span>
+                </form>
+
+
+                <h6> Subscribe to <a>Mori Calliope's channel</a></h6>
+            </div>
+        </div>
+        <div id="credits">
+
+        </div>
+    </body>
+</html>
