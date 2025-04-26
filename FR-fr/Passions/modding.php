fe8a6d992fcb3d8e773ffcd351475c74f68c006e
Badreddine <badreddinerezzouk@protonmail.com>
2025-04-27 01:21:17 +0200

    Ajout de la mise à jour sur la situation Sinder

--- FR-fr/Passions/modding.php
+++ FR-fr/Passions/modding.php
@@ -60,7 +60,7 @@ $title = 'Modding - Badreddine Rezzouk';
             <div class="alert alert-danger">
                 Mise à jour du 27 avril : <br>
                 Il est apparu que Sinder et son (ancien) manager REDACTED ont commis des actes odieux à l'encontre de la créatrice Nanoless et de plusieurs streameurs/ses virtuel(le)s, je désapprouve totalement ce type de comportement. Après réflexion, j'ai pris la décision suivante : <br>
-                Les mods la concernant resteront sur la page du portfolio et de modworkshop, aucun autre mod ne sera créé à son image.
+                Les mods la concernant resteront sur la page du portfolio et de modworkshop, aucun autre mod ne sera créé à son image. <br>
                 Merci de votre compréhension.
             </div>
             <details class="text-start">
