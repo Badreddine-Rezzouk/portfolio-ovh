3f23b6ad5581990323e238003da70cddd3b333b8
Badreddine <badreddinerezzouk@protonmail.com>
2025-05-22 10:47:13 +0200

    MàJ Calliguh

--- calliguh/index.php
+++ calliguh/index.php
@@ -4,19 +4,14 @@
 <html lang="en">
     <head>
         <meta charset="UTF-8">
-        <?php require_once '../Common-files/header.php'?>
-        <style>
-            #clickButton {
-                background: none;
-                border: none;
-                padding: 0;
-                cursor: pointer;
-            }
-        </style>
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+        <link rel="stylesheer" href="CSS/stylesheet.css">
     </head>
     <body>
         <div class="width-box" style="background: url('./bg-calliguh.png'); background-size: cover; min-height: 80vh">
             <div class="box"></div>
+            <div class="row"></div>
             <div class="bg-white p-4 rounded-3 text-center w-50 mx-auto border border-5 shadow-lg">
                 <h4>Global Counter</h4>
                 <h1 id="counter">0</h1>
