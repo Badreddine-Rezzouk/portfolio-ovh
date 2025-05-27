0fc5d5ce2d31c155a9ce9a99e0ca4d486df4ce40
Badreddine <badreddinerezzouk@protonmail.com>
2025-05-27 11:05:18 +0200

    MàJ Calliguh + page test

--- FR-fr/page_test.php
+++ FR-fr/page_test.php
@@ -11,7 +11,7 @@
         <h1> Vous n'êtes pas censé être là </h1>
         <p> Cet espace me sert de petits tests pour l'implémentation de features.</p>
 
-
+        <iframe width="50vw" height="50vh" class="twitter-timeline" data-dnt="true" data-theme="dark" src="https://twitter.com/SinderVTuber?ref_src=twsrc%5Etfw"></iframe>
         <a class="twitter-timeline" data-dnt="true" data-theme="dark" href="https://twitter.com/SinderVTuber?ref_src=twsrc%5Etfw">Tweets by SinderVTuber</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
     </body>
 </html>
