7a72b203f10c1ba6603a0b5c7deba26b91eef484
Badreddine <badreddinerezzouk@protonmail.com>
2025-05-27 11:11:12 +0200

    MàJ Calliguh + page test

--- FR-fr/page_test.php
+++ FR-fr/page_test.php
@@ -13,7 +13,7 @@ $title = "Page Test - Badreddine Rezzouk";
     <div class="gradient-box">
         <h1> Vous n'êtes pas censé être là </h1>
         <p> Cet espace me sert de petits tests pour l'implémentation de features.</p>
-        <iframe width="50vw" height="50vh" class="twitter-timeline" data-dnt="true" data-theme="dark" src="https://twitter.com/SinderVTuber?ref_src=twsrc%5Etfw"></iframe>
+        <iframe width="400px" height="600px" class="twitter-timeline" data-dnt="true" data-theme="dark" src="https://twitter.com/SinderVTuber?ref_src=twsrc%5Etfw"></iframe>
         <a class="twitter-timeline" data-dnt="true" data-theme="dark" href="https://twitter.com/SinderVTuber?ref_src=twsrc%5Etfw">Tweets by SinderVTuber</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
     </div>
     <?php require "../Common-files/footer.php"; ?>
