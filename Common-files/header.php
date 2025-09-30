0d1f4019b8c64c8ef8dabe47102aed646ea11f64
Badreddine <badreddinerezzouk@protonmail.com>
2025-09-30 10:22:32 +0200

    boom

--- Common-files/header.php
+++ Common-files/header.php
@@ -32,20 +32,20 @@ $topURL = rtrim($protocol . $host) . '/';
 
 <meta charset="utf-8"></meta>
 <title><?php echo isset($title) ? htmlspecialchars($title) : 'Portfolio'; ?></title>
+<link rel="icon" href="<?php echo $topURL?>favicon.ico" type="image/x-icon">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
 <link href="<?php echo $topURL?>CSS/Coloring.css" rel="stylesheet">
 <link href="<?php echo $topURL?>CSS/Bootstrap.css" rel="stylesheet">
 <link href="<?php echo $topURL?>CSS/shaping.css" rel="stylesheet">
 <link href="<?php echo $topURL?>CSS/proficiency_bars.css" rel="stylesheet">
 <link href="<?php echo $topURL?>CSS/animation.css" rel="stylesheet">
-<link rel="icon" href="<?php echo $topURL?>favicon.ico" type="image/x-icon">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
 <script src="<?php echo $topURL?>JS/summary-animation.js"></script>
 <script src="<?php echo $topURL?>JS/cookie_base.js"></script>
-
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/luxon@3.3.0/build/global/luxon.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/masonry-layout@4.2.2/dist/masonry.pkgd.min.js" integrity="sha384-GNFwBvfVxBkLMJpYMOABq3c+d3KnQxudP/mGPkzpZSTYykLBNsZEnG2D9G/X/+7D" crossorigin="anonymous" async></script>
+
 
 
